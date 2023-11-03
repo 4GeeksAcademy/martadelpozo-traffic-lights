e8a76a19896f119f1ca15845aeeff9ae498f08a4
martadelpozo <138892528+martadelpozo@users.noreply.github.com>
2023-11-03 20:35:44 +0000

    change color button

--- src/js/component/traffic-light.jsx
+++ src/js/component/traffic-light.jsx
@@ -1,20 +1,33 @@
-import React, {useState} from 'react'
-
+import React, { useState } from 'react';
 
 function TrafficLight() {
-    const [color, setColor] = useState('red');
-  
-    const handleClick = (newColor) => {
-      setColor(newColor);
-    };
-  
-    return (
-      <div className="container">
-        <div className={`red light ${color === 'red' ? 'glow' : ''}`} onClick={() => handleClick('red')}></div>
-        <div className={`yellow light ${color === 'yellow' ? 'glow' : ''}`} onClick={() => handleClick('yellow')}></div>
-        <div className={`green light ${color === 'green' ? 'glow' : ''}`} onClick={() => handleClick('green')}></div>
-      </div>
-    );
-  }
+  const colors = ['red', 'yellow', 'green'];
+  const [colorIndex, setColorIndex] = useState(0);
+
+  const handleClick = () => {
+    const nextColorIndex = (colorIndex + 1) % colors.length;
+    setColorIndex(nextColorIndex);
+  };
+
+  return (
+    <div className="container">
+      <div
+        className={`red light ${colors[colorIndex] === 'red' ? 'glow' : ''}`}
+        onClick={() => handleClick('red')}
+      ></div>
+      <div
+        className={`yellow light ${colors[colorIndex] === 'yellow' ? 'glow' : ''}`}
+        onClick={() => handleClick('yellow')}
+      ></div>
+      <div
+        className={`green light ${colors[colorIndex] === 'green' ? 'glow' : ''}`}
+        onClick={() => handleClick('green')}
+      ></div>
+           <button onClick={handleClick}>Change Color</button>
+    </div>
+
+  );
+}
 
 export default TrafficLight;
+
